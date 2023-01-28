fc5a1c0ecc9be6c78b2904ea00f91ee48fb5e959
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2023-01-29 02:38:39 +0530

    okk33

--- src/Components/CustomNavbar.jsx
+++ src/Components/CustomNavbar.jsx
@@ -17,7 +17,7 @@ const handleScroll=()=>{
  <Navbar collapseOnSelect expand="lg" bg="white" sticky='top'   style={{width:"100%"}}>
 
         <Navbar.Brand href="/" className='' 
-        style={{paddingLeft:"25px",fontFamily: 'Playfair Display, serif',fontSize:"5vH",color: "hsl(240, 38%, 20%)"}}
+        style={{paddingLeft:"25px",fontFamily: 'Playfair Display, serif',fontSize:"4.2vH",color: "hsl(240, 38%, 20%)"}}
         >
          Artist
          </Navbar.Brand>
@@ -33,33 +33,33 @@ const handleScroll=()=>{
    
             <Nav.Link>
               {' '}
-              <Link className="text-decoration-none text-black" to="/" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"3vH",color: "hsl(240, 38%, 20%)"}}>
+              <Link className="text-decoration-none text-black" to="/" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"2.8vH",color: "hsl(240, 38%, 20%)"}}>
                 Home
               </Link>
             </Nav.Link>
 
             <Nav.Link>
               {' '}
-              <HashLink className="text-decoration-none text-black" to="/#whatwedo" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"3vH",color: "hsl(240, 38%, 20%)"}}>
+              <HashLink className="text-decoration-none text-black" to="/#whatwedo" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"2.8vH",color: "hsl(240, 38%, 20%)"}}>
                 WhatweDo
               </HashLink>
             </Nav.Link>
             <Nav.Link>
               {' '}
-              <Link className="text-decoration-none text-black" to="/gallery" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"3vH",color: "hsl(240, 38%, 20%)"}}>
+              <Link className="text-decoration-none text-black" to="/gallery" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"2.8vH",color: "hsl(240, 38%, 20%)"}}>
                 Gallery
               </Link>
             </Nav.Link>
             <Nav.Link>
               {' '}
-              <Link className="text-decoration-none text-black" to="/aboutus" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"3vH",color: "hsl(240, 38%, 20%)"}}>
+              <Link className="text-decoration-none text-black" to="/aboutus" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"2.8vH",color: "hsl(240, 38%, 20%)"}}>
                 About
               </Link>
             </Nav.Link>
 
             <Nav.Link>
               {' '}
-              <Link className="text-decoration-none text-black" to="/contact" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"3vH",color: "hsl(240, 38%, 20%)"}}>
+              <Link className="text-decoration-none text-black" to="/contact" style={{fontFamily: 'Barlow Condensed, sans-serif',fontSize:"2.8vH",color: "hsl(240, 38%, 20%)"}}>
                 Contact Us
               </Link>
               </Nav.Link>
